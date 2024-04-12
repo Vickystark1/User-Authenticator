0468973498ee46c0c007b36f46b514a970d8b038
vignesh <vickystark737@gmail.com>
2024-04-12 13:35:21 +0530

    check

---
+++ src/components/NavBar.js
@@ -0,0 +1,209 @@
+import { Link } from "react-router-dom";
+import { isAuthenticated } from "../services/Auth";
+import "./NavBar.css";
+
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { ThemeProvider } from "@mui/material";
+
+const drawerWidth = 240;
+const navItems = ["Register", "Login", "Dashboard", "Logout"];
+
+export default function NavBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <a className="navbar-brand" href="#" style={{ textDecoration: "none" }}>
+        <Typography variant="h6" sx={{ my: 2 }}>
+          User Authenticator
+        </Typography>
+      </a>
+      {/* <a  className="navbar-brand" href="#" ><h6>User Authenticator</h6></a> */}
+
+      <Divider />
+      <List style={{ color: "black" }}>
+        {!isAuthenticated() ? (
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText>
+                <Link className="nav-link" to="/">
+                  Register
+                </Link>
+              </ListItemText>
+            </ListItemButton>
+          </ListItem>
+        ) : null}
+        {!isAuthenticated() ? (
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText>
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </ListItemText>
+            </ListItemButton>
+          </ListItem>
+        ) : null}
+        {isAuthenticated() ? (
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText>
+                <Link className="nav-link" to="/dashboard">
+                  Dashboard
+                </Link>
+              </ListItemText>
+            </ListItemButton>
+          </ListItem>
+        ) : null}
+        {isAuthenticated() ? (
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText>
+                <a
+                  className="nav-link"
+                  onClick={props.logoutUser}
+                  style={{ cursor: "pointer" }}
+                >
+                  Logout
+                </a>
+              </ListItemText>
+            </ListItemButton>
+          </ListItem>
+        ) : null}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <div>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar component="nav">
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
+              style={{ justifyContent: "end" }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+            >
+              User Authenticator
+            </Typography>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              {!isAuthenticated() ? (
+                <Button sx={{ color: "#fff" }}>
+                  <Link className="nav-link" style={{ color: "white" }} to="/">
+                    Register
+                  </Link>
+                </Button>
+              ) : null}
+              {!isAuthenticated() ? (
+                <Button sx={{ color: "#fff" }}>
+                  <Link
+                    className="nav-link"
+                    style={{ color: "white" }}
+                    to="/login"
+                  >
+                    Login
+                  </Link>
+                </Button>
+              ) : null}
+              {isAuthenticated() ? (
+                <Button sx={{ color: "#fff" }}>
+                  <Link
+                    className="nav-link"
+                    style={{ color: "white" }}
+                    to="/dashboard"
+                  >
+                    Dashboard
+                  </Link>
+                </Button>
+              ) : null}
+              {isAuthenticated() ? (
+                <Button sx={{ color: "#fff" }}>
+                  <a
+                    className="nav-link"
+                    onClick={props.logoutUser}
+                    style={{ cursor: "pointer", color: "white" }}
+                  >
+                    Logout
+                  </a>
+                </Button>
+              ) : null}
+            </Box>
+          </Toolbar>
+        </AppBar>
+        
+        <nav>
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </nav>
+        <Box component="main" sx={{ p: 3 }}>
+          <Toolbar />
+          {/* <Typography>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique unde
+          fugit veniam eius, perspiciatis sunt? Corporis qui ducimus quibusdam,
+          aliquam dolore excepturi quae. Distinctio enim at eligendi perferendis in
+          cum quibusdam sed quae, accus
+        </Typography> */}
+        </Box>
+        
+      </Box>
+    </div>
+  );
+}
+
+NavBar.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react"
+import { useNavigate, Navigate } from "react-router-dom"
+import NavBar from "../components/NavBar"
+import { UserDetailsApi } from "../services/Api"
+import { logout,isAuthenticated } from "../services/Auth"
+import React from "react";
+import BannerImage from "../Assets/home-banner-image.jpg";
+import { FiArrowRight } from "react-icons/fi";
+import tick from "../Assets/tick.gif";
+
+export default function DashboardPage(){
+    const navigate = useNavigate();
+
+    const [user,setUser] = useState({name:"",email:"",localId:""})
+
+    useEffect(()=>{
+        if(isAuthenticated()){
+            UserDetailsApi().then((response)=>{
+               
+                setUser({
+                    name:response.data.users[0].displayName,
+                    email:response.data.users[0].email,
+                    localId:response.data.users[0].localId,
+                })
+            })
+        }
+    },[])
+
+    const logoutUser = ()=>{
+        logout();
+        navigate('/login')
+    }
+
+    if (!isAuthenticated()) {
+        //redirect user to dashboard
+        return <Navigate to="/login" />
+    }
+
+    return (
+        <div className="home-container text-center">
+      <NavBar logoutUser={logoutUser}  />
+      <div className='d-flex justify-content-center align-items-center' style={{position:"relative",top:"30vh",alignItems:"center"}} >
+      <div style={{display:"flex",position:"relative",height:"15vh",padding:"20px 20px",border:"1px dashed gray",alignItems:"center",justifyContent:"center"}}>
+      <img src={tick} alt ="" style={{height:80,Width:80,position:'absolute',top:-40,borderRadius:"50px 50px"}}/>
+      <h2 style={{position:"relative",top:"3vh"}}>Successfully Logedin!..</h2>
+
+
+      </div>
+      </div>
+    </div>
+    )
+}
